5cddf625eb5e42c514ee885cc339ead17a48716a
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-20 19:10:28 +0200

    added db context, repo & migrations

---
+++ BE-WOK-platform/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>(attr =>
+            {
+                attr.HasKey("Id");
+
+                attr.Property(x => x.Name)
+                    .HasMaxLength(50);
+            });
+        }
+
+    }
+}
---
+++ BE-WOK-platform/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using Application.Interfaces;
+using Domain.Models;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(
+            AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            _context.Categories.Add(category);
+
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category?> Get(Guid id)
+        {
+            return await _context.Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<IEnumerable<Category>> GetAll()
+        {
+            return await _context.Categories
+                .ToListAsync();
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            _context.Categories.Update(category);
+
+            await _context.SaveChangesAsync();
+
+            return category;    
+        }
+    }
+}
