1a18edf4f1660891e40b1d7b6e703d8e4f9329de
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-20 19:10:28 +0200

    added db context, repo & migrations

---
+++ BE-WOK-platform/Application/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace Application.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category?> Get(Guid id);
+        Task<IEnumerable<Category>> GetAll();
+
+    }
+}
