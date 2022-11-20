2c46d0683221a1bca64f90db35059b7cab979aae
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-20 19:10:55 +0200

    added commands & queries for the model

---
+++ BE-WOK-platform/Application/Categories/Queries/GetCategoryById/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Categories.Queries.GetCategoryById
+{
+    public class GetCategoryByIdQueryHandler : IRequestHandler<GetCategoryByIdQuery, Category>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public GetCategoryByIdQueryHandler(
+            ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<Category> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepository.Get(request.Id)
+                ?? throw new ObjectNotFoundException(
+                    nameof(Category),
+                    request.Id);
+
+            return category;
+        }
+    }
+}
