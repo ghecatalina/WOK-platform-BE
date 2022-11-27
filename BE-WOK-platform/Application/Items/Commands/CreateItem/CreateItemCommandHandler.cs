455e8a3dc51ff574323828537b2d6b1f36f29764
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:34:50 +0200

    added create item command

---
+++ BE-WOK-platform/Application/Items/Commands/CreateItem/CreateItemCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Items.Commands.CreateItem
+{
+    public class CreateItemCommandHandler : IRequestHandler<CreateItemCommand, Item>
+    {
+        private readonly IItemRepository _itemRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CreateItemCommandHandler(
+            IItemRepository itemRepository, 
+            ICategoryRepository categoryRepository)
+        {
+            _itemRepository = itemRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<Item> Handle(CreateItemCommand request, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepository.Get(request.CategoryId, cancellationToken)
+                ?? throw new ObjectNotFoundException(
+                    nameof(Category),
+                    request.CategoryId);
+
+            var item = new Item
+            {
+                Name = request.Name,
+                Quantity = request.Quantity,
+                Ingredients = request.Ingredients,
+                Description = request.Description,
+                Photo = request.Photo,
+                CategoryId = category.Id,
+                Category = category,
+            };
+
+            return await _itemRepository.Create(item, cancellationToken);
+        }
+    }
+}
