5f44091f21a900519b8a9407e0afb381c5f823b2
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:35:36 +0200

    added get item by id query

---
+++ BE-WOK-platform/Application/Items/Queries/GetItemById/GetItemByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.Items.Queries.GetItemById
+{
+    public class GetItemByIdQuery : IRequest<Item>
+    {
+        public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
