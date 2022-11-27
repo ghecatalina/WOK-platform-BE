fd4029919e732100cea613b992b745f7315e160d
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 18:39:53 +0200

    added dtos and profile for daily menu

---
+++ BE-WOK-platform/API/DTOs/DailyMenu/DailyMenuGetModel.cs
@@ -0,0 +1,11 @@
+﻿using API.DTOs.Items;
+
+namespace API.DTOs.DailyMenu
+{
+    public class DailyMenuGetModel
+    {
+        public ItemGetModel FirstDish { get; set; }
+        public ItemGetModel SecondDish { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ BE-WOK-platform/API/DTOs/DailyMenu/DailyMenuPutModel.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTOs.DailyMenu
+{
+    public class DailyMenuPutModel
+    {
+        public Guid FirstDish { get; set; }
+        public Guid SecondDish { get; set; }
+        public double Price { get; set; }
+    }
+}
