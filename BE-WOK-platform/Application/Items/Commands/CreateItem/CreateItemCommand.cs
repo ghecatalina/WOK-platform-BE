374ef667f4299dc5f633634d761ea4323d6a205b
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:34:50 +0200

    added create item command

---
+++ BE-WOK-platform/Application/Items/Commands/CreateItem/CreateItemCommand.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.Items.Commands.CreateItem
+{
+    public class CreateItemCommand : IRequest<Item>
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Ingredients { get; set; }
+        public string? Description { get; set; }
+        public string Photo { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
