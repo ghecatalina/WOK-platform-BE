6632a2c51e9f9aee590cea80b6c0b9ab3e50260b
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 18:39:53 +0200

    added dtos and profile for daily menu

---
+++ BE-WOK-platform/API/Profiles/DailyMenuProfile.cs
@@ -0,0 +1,14 @@
+﻿using API.DTOs.DailyMenu;
+using AutoMapper;
+using Domain.Models;
+
+namespace API.Profiles
+{
+    public class DailyMenuProfile : Profile
+    {
+        public DailyMenuProfile()
+        {
+            CreateMap<DailyMenu, DailyMenuGetModel>();
+        }
+    }
+}
