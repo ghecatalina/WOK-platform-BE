b944163c77509bc4d94353740f1eaa799121dd33
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:36:31 +0200

    imporvement on object not found exception

--- BE-WOK-platform/Application/Exceptions/ObjectNotFoundException.cs
+++ BE-WOK-platform/Application/Exceptions/ObjectNotFoundException.cs
@@ -7,5 +7,17 @@
         {
 
         }
+
+        public ObjectNotFoundException(string type, string key, object value)
+            : base($"Object of type {type} with {key}:{value} doesn't exist.")
+        {
+
+        }
+
+        public ObjectNotFoundException(string type, params (string key, object value)[] keyFields)
+            : base($"Object of type {type} with {string.Join(" and ", keyFields.Select(p => $"{p.key}:{p.value}"))} doesn't exist.")
+        {
+
+        }
     }
 }
