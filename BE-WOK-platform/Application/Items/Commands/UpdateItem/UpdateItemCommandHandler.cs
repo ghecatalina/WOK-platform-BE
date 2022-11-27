4921f977f9c0bd949398308cd282e8177a100a86
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:35:11 +0200

    added update item command

---
+++ BE-WOK-platform/Application/Items/Commands/UpdateItem/UpdateItemCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Items.Commands.UpdateItem
+{
+    public class UpdateItemCommandHandler : IRequestHandler<UpdateItemCommand, Item>
+    {
+        private readonly IItemRepository _itemRepository;
+
+        public UpdateItemCommandHandler(
+            IItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
+
+        public async Task<Item> Handle(UpdateItemCommand request, CancellationToken cancellationToken)
+        {
+            var item = await _itemRepository.Get(request.CategoryId, request.Id, cancellationToken)
+                ?? throw new ObjectNotFoundException(
+                    nameof(Item),
+                    (nameof(Item.CategoryId), request.CategoryId),
+                    (nameof(Item.Id), request.Id));
+
+            item.Name= request.Name;
+            item.Description= request.Description;
+            item.Quantity= request.Quantity;
+            item.Photo = request.Photo;
+            item.Ingredients= request.Ingredients;
+
+            return await _itemRepository.Update(item, cancellationToken);
+        }
+    }
+}
