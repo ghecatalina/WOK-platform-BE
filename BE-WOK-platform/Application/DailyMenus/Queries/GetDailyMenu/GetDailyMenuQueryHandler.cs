cb5bc478ed08599727d603073003d37fd4c211de
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 18:39:13 +0200

    added daily menu get query

---
+++ BE-WOK-platform/Application/DailyMenus/Queries/GetDailyMenu/GetDailyMenuQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+
+namespace Application.DailyMenus.Queries.GetDailyMenu
+{
+    public class GetDailyMenuQueryHandler : IRequestHandler<GetDailyMenuQuery, DailyMenu>
+    {
+        private readonly IDailyMenuRepository _dailyMenuRepository;
+
+        public GetDailyMenuQueryHandler(
+            IDailyMenuRepository dailyMenuRepository)
+        {
+            _dailyMenuRepository = dailyMenuRepository;
+        }
+
+        public async Task<DailyMenu> Handle(GetDailyMenuQuery request, CancellationToken cancellationToken)
+        {
+            var dailyMenu = await _dailyMenuRepository.Get(cancellationToken)
+                ?? throw new ObjectNotFoundException(nameof(DailyMenu));
+
+            return dailyMenu;
+        }
+    }
+}
