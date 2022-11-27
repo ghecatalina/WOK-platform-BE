925da48db2e28fd78bb7d53041c24650f96463be
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:35:01 +0200

    added delete item command

---
+++ BE-WOK-platform/Application/Items/Commands/DeleteItem/DeleteItemCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Items.Commands.DeleteItem
+{
+    public class DeleteItemCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
