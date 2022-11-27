4894f321fccd6efd44577ccf909e3c8a521d95b8
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:36:15 +0200

    item profile

---
+++ BE-WOK-platform/API/Profiles/ItemProfile.cs
@@ -0,0 +1,19 @@
+﻿using API.DTOs.Items;
+using Application.Items.Commands.CreateItem;
+using Application.Items.Commands.UpdateItem;
+using AutoMapper;
+using Domain.Models;
+
+namespace API.Profiles
+{
+    public class ItemProfile : Profile
+    {
+        public ItemProfile()
+        {
+            CreateMap<ItemPostPutModel, Item>();
+            CreateMap<Item, ItemGetModel>();
+            CreateMap<ItemPostPutModel, CreateItemCommand>();
+            CreateMap<ItemPostPutModel, UpdateItemCommand>();
+        }
+    }
+}
