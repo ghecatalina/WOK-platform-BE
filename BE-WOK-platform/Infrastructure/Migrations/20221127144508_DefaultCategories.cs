ad9a6a4a67562e62f95a2f94fc5957729a648a04
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:46:53 +0200

    added default categories migrations

---
+++ BE-WOK-platform/Infrastructure/Migrations/20221127144508_DefaultCategories.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DefaultCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("00000000-0000-0000-0000-000000000001"), "Salads & Starters" },
+                    { new Guid("00000000-0000-0000-0000-000000000002"), "Soups" },
+                    { new Guid("00000000-0000-0000-0000-000000000003"), "Main Courses" },
+                    { new Guid("00000000-0000-0000-0000-000000000004"), "Desserts" },
+                    { new Guid("00000000-0000-0000-0000-000000000005"), "Drinks" },
+                    { new Guid("00000000-0000-0000-0000-000000000006"), "Coffee & Tea" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000003"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000004"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000005"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000006"));
+        }
+    }
+}
---
+++ BE-WOK-platform/Infrastructure/SeedData/DefaultCategories.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+
+namespace Infrastructure.SeedData
+{
+    public static class DefaultCategories
+    {
+        public static List<Category> GetDefaultCategories()
+        {
+            return new List<Category>()
+            {
+                new Category { Id = new Guid("00000000-0000-0000-0000-000000000001"), Name = "Salads & Starters"},
+                new Category { Id = new Guid("00000000-0000-0000-0000-000000000002"), Name = "Soups"},
+                new Category { Id = new Guid("00000000-0000-0000-0000-000000000003"), Name = "Main Courses"},
+                new Category { Id = new Guid("00000000-0000-0000-0000-000000000004"), Name = "Desserts"},
+                new Category { Id = new Guid("00000000-0000-0000-0000-000000000005"), Name = "Drinks"},
+                new Category { Id = new Guid("00000000-0000-0000-0000-000000000006"), Name = "Coffee & Tea"}
+            };
+        }
+    }
+}
