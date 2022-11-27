76629405f3e5d984de00d6cd5f6e8b4095e09978
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:35:53 +0200

    added get items by category query

---
+++ BE-WOK-platform/Application/Items/Queries/GetItemsByCategory/GetItemsByCategoryQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Items.Queries.GetItemsByCategory
+{
+    public class GetItemsByCategoryQueryHandler : IRequestHandler<GetItemsByCategoryQuery, IEnumerable<Item>>
+    {
+        private readonly IItemRepository _itemRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public GetItemsByCategoryQueryHandler(
+            IItemRepository itemRepository, 
+            ICategoryRepository categoryRepository)
+        {
+            _itemRepository = itemRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IEnumerable<Item>> Handle(GetItemsByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepository.Get(request.CategoryId, cancellationToken)
+                ?? throw new ObjectNotFoundException(
+                    nameof(Category),
+                    request.CategoryId);
+
+            return await _itemRepository.GetAll(request.CategoryId, cancellationToken);
+        }
+    }
+}
