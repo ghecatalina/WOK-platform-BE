17cca5bbddf7da1ca096c273c61119edbc523803
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:36:04 +0200

    added items dtos

---
+++ BE-WOK-platform/API/DTOs/Items/ItemGetModel.cs
@@ -0,0 +1,13 @@
+﻿namespace API.DTOs.Items
+{
+    public class ItemGetModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Ingredients { get; set; }
+        public string? Description { get; set; }
+        public string Photo { get; set; }
+        // public Guid CategoryId { get; set; }
+    }
+}
