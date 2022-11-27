489358e58be40ec6238d151e9c4532ea41c6608e
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:35:11 +0200

    added update item command

---
+++ BE-WOK-platform/Application/Items/Commands/UpdateItem/UpdateItemCommand.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.Items.Commands.UpdateItem
+{
+    public class UpdateItemCommand : IRequest<Item>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Ingredients { get; set; }
+        public string? Description { get; set; }
+        public string Photo { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
