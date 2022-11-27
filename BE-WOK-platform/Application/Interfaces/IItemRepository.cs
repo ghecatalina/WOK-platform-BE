43e888c124268c225a0cd05fc3a02613846838de
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:34:02 +0200

    added item repo

---
+++ BE-WOK-platform/Application/Interfaces/IItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace Application.Interfaces
+{
+    public interface IItemRepository
+    {
+        Task<Item> Create(Item item, CancellationToken ct);
+        Task<Item> Update(Item item, CancellationToken ct);
+        Task<Item?> Get(Guid categoryid, Guid itemId, CancellationToken ct);
+        Task<IEnumerable<Item>> GetAll(Guid categoryId, CancellationToken ct);
+        Task Delete(Item item, CancellationToken ct);
+    }
+}
