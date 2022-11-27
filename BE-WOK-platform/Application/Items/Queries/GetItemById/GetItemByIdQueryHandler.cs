76aa33396eac0e02624dc46fcb44de400800dba6
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 16:35:36 +0200

    added get item by id query

---
+++ BE-WOK-platform/Application/Items/Queries/GetItemById/GetItemByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Items.Queries.GetItemById
+{
+    public class GetItemByIdQueryHandler : IRequestHandler<GetItemByIdQuery, Item>
+    {
+        private readonly IItemRepository _itemRepository;
+
+        public GetItemByIdQueryHandler(
+            IItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
+
+        public async Task<Item> Handle(GetItemByIdQuery request, CancellationToken cancellationToken)
+        {
+            var item = await _itemRepository.Get(request.CategoryId, request.Id, cancellationToken)
+                ?? throw new ObjectNotFoundException(
+                    nameof(Item),
+                    (nameof(Item.CategoryId), request.CategoryId),
+                    (nameof(Item.Id), request.Id));
+
+            return item;
+        }
+    }
+}
