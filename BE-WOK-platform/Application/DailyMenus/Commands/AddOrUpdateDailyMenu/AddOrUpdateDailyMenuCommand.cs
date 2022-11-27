de783e11d47b9b4eb77e5e964f3505bfab974d4e
ghecatalina <gheorghioiu.catalina@gmail.com>
2022-11-27 18:38:55 +0200

    added daily menu create or update command

---
+++ BE-WOK-platform/Application/DailyMenus/Commands/AddOrUpdateDailyMenu/AddOrUpdateDailyMenuCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.DailyMenus.Commands.AddOrUpdateDailyMenu
+{
+    public class AddOrUpdateDailyMenuCommand : IRequest<Unit>
+    {
+        public Guid FirstDish { get; set; }
+        public Guid SecondDish { get; set; }
+        public double Price { get; set; }
+    }
+}
