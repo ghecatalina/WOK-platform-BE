09963002ebdb49fbf5fae3767c4637dd57a709a8
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-15 22:30:41 +0200

    added invalid date and duplicate item exceptions

---
+++ BE-WOK-platform/Application/Exceptions/DuplicateItemException.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Application.Exceptions
+{
+    public class DuplicateItemException : ApplicationException
+    {
+        public DuplicateItemException(string itemType, string itemKey, object itemValue)
+            : base($"Object of type {itemType} with {itemKey}:{itemValue} already exists.")
+        {
+        }
+
+        public DuplicateItemException(
+            string itemType,
+            Dictionary<string, object> itemFields)
+            : base($"Object of type {itemType} with " + ConvertDictionaryToString(itemFields) + " already exists.")
+        {
+        }
+
+        private static string ConvertDictionaryToString(Dictionary<string, object> dict)
+        {
+            StringBuilder sb = new();
+
+            foreach (KeyValuePair<string, object> kvp in dict)
+            {
+                sb.Append(kvp.Key);
+                sb.Append(':');
+                sb.Append(kvp.Value);
+                sb.Append(',');
+            }
+
+            return sb.ToString().TrimEnd(',');
+        }
+    }
+}
---
+++ BE-WOK-platform/Application/Exceptions/InvalidDateTimeException.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Exceptions
+{
+    public class InvalidDateTimeException : ApplicationException
+    {
+        public InvalidDateTimeException(DateTime date1)
+            : base($"DateTime {date1} less than current time {DateTime.UtcNow}.")
+        { }
+    }
+}
