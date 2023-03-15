54214b322001313d6dd3a0dd8b98f4f9d5588bfe
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-15 22:30:19 +0200

    added reservation repo

---
+++ BE-WOK-platform/Application/Interfaces/IReservationRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Application.Interfaces
+{
+    public interface IReservationRepository
+    {
+        Task<Reservation> Create(Reservation reservation, CancellationToken ct);
+        Task<IEnumerable<Reservation>> GetReservations(CancellationToken ct);
+        Task<Reservation?> GetByTableAndDate(int tableNumber, DateTime date, CancellationToken ct);
+    }
+}
