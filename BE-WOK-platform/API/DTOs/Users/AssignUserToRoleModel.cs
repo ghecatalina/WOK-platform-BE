2ef65b0f320d1d11c4e8937c56304bb740a2b431
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-19 23:42:40 +0200

    users dtos

---
+++ BE-WOK-platform/API/DTOs/Users/AssignUserToRoleModel.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs.Users
+{
+    public class AssignUserToRoleModel
+    {
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+    }
+}
---
+++ BE-WOK-platform/API/DTOs/Users/UserloginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs.Users
+{
+    public class UserloginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
