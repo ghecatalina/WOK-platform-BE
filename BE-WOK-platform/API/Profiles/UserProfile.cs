ba44efad194899fcff4f5bd82422bf3dd4d34864
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-19 23:42:49 +0200

    user profile

---
+++ BE-WOK-platform/API/Profiles/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using API.DTOs.Users;
+using Application.Users.Commands.AssignRoleToUser;
+using Application.Users.Commands.RegisterUser;
+using Application.Users.Queries.LoginUser;
+using AutoMapper;
+
+namespace API.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserRegisterModel, RegisterUserCommand>();
+            CreateMap<UserloginModel, LoginUserQuery>();
+            CreateMap<AssignUserToRoleModel, AssignRoleToUserCommand>();
+        }
+    }
+}
