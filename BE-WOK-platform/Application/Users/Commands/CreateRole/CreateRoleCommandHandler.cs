686f63e7d899e8db6bc257cce12344f2f9ec251e
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-19 23:42:02 +0200

    create role app

---
+++ BE-WOK-platform/Application/Users/Commands/CreateRole/CreateRoleCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Users.Commands.CreateRole
+{
+    public class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommand, string?>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public CreateRoleCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<string?> Handle(CreateRoleCommand request, CancellationToken cancellationToken)
+        {
+            var existingRole = await _userRepository.GetRoleByName(request.Name);
+            if (existingRole != null)
+            {
+                throw new DuplicateItemException(
+                    nameof(Role),
+                    nameof(Role.Name),
+                    request.Name);
+            }
+
+            var result = await _userRepository.CreateRole(new Role { Name = request.Name });
+            return result;
+        }
+    }
+}
