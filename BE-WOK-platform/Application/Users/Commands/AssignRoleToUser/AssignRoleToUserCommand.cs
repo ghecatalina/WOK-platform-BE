77e4ccc36f7bd2c1db46051afb64ecc5a040dd6e
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-19 23:42:14 +0200

    assign user to role app

---
+++ BE-WOK-platform/Application/Users/Commands/AssignRoleToUser/AssignRoleToUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Users.Commands.AssignRoleToUser
+{
+    public class AssignRoleToUserCommand : IRequest<string?>
+    {
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+    }
+}
