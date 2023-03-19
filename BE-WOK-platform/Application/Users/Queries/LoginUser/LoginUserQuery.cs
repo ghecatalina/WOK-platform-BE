b10838144dacc38ab11a7be929b55834225cc5e6
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-19 23:41:45 +0200

    login user app

---
+++ BE-WOK-platform/Application/Users/Queries/LoginUser/LoginUserQuery.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.Users.Queries.LoginUser
+{
+    public class LoginUserQuery : IRequest<(User User, IEnumerable<string> Roles)>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
