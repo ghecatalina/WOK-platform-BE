ec2f217bdb61ec8ff6526dedf575d2552b7673bc
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-19 23:42:02 +0200

    create role app

---
+++ BE-WOK-platform/Application/Users/Commands/CreateRole/CreateRoleCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Users.Commands.CreateRole
+{
+    public class CreateRoleCommand : IRequest<string?>
+    {
+        public string Name { get; set; }
+    }
+}
