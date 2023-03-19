76d1b3fe92cb3e4aa2978cb9027b04a8b0901872
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-19 23:42:14 +0200

    assign user to role app

---
+++ BE-WOK-platform/Application/Users/Commands/AssignRoleToUser/AssignRoleToUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+using Domain.Models;
+using MediatR;
+
+namespace Application.Users.Commands.AssignRoleToUser
+{
+    public class AssignRoleToUserCommandHandler : IRequestHandler<AssignRoleToUserCommand, string?>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AssignRoleToUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<string?> Handle(AssignRoleToUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByEmail(request.Email)
+                ?? throw new ObjectNotFoundException(
+                    nameof(User),
+                    request.Email);
+
+            var role = await _userRepository.GetRoleByName(request.RoleName)
+                ?? throw new ObjectNotFoundException(
+                    nameof(Role),
+                    request.RoleName);
+
+            var result = await _userRepository.AddUserToRole(user, role);
+            return result;
+        }
+    }
+}
