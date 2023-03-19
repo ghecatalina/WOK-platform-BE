3b9ee401e9f4225bcf2fd0bc7ea63f5a37e7795f
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-19 23:41:23 +0200

    added user repo

---
+++ BE-WOK-platform/Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+
+namespace Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<string?> Register(User user, string password);
+        Task<User?> GetByEmail(string email);
+        Task<bool> CheckPassword(User user, string password);
+        Task<IEnumerable<string>> GetRoleByUser(User user);
+        Task<Role?> GetRoleByName(string name);
+        Task<string?> CreateRole(Role role);
+        Task<string?> AddUserToRole(User user, Role role);
+    }
+}
---
+++ BE-WOK-platform/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,68 @@
+﻿using Application.Interfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+
+        public UserRepository(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<string?> Register(User user, string password)
+        {
+            var userCreatedResult = await _userManager.CreateAsync(user, password);
+            if (userCreatedResult.Succeeded)
+            {
+                return null;
+            }
+            return userCreatedResult.Errors.First().Description;
+        }
+
+        public async Task<User?> GetByEmail(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        } 
+
+        public async Task<bool> CheckPassword(User user, string password)
+        {
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
+
+        public async Task<IEnumerable<string>> GetRoleByUser(User user)
+        {
+            return await _userManager.GetRolesAsync(user);
+        }
+
+        public async Task<Role?> GetRoleByName(string name)
+        {
+            return await _roleManager.FindByNameAsync(name);
+        }
+
+        public async Task<string?> CreateRole(Role role)
+        {
+            var createdRole = await _roleManager.CreateAsync(role);
+            if (createdRole.Succeeded)
+            {
+                return null;
+            }
+            return createdRole.Errors.First().Description;
+        }
+
+        public async Task<string?> AddUserToRole(User user, Role role)
+        {
+            var result = await _userManager.AddToRoleAsync(user, role.Name);
+            if (result.Succeeded)
+            {
+                return null;
+            }
+            return result.Errors.First().Description;
+        }
+    }
+}
