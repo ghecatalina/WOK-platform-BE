07a1fae1d6795c7898ede8a64382cc1387926207
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-26 16:19:57 +0300

    extended reservation model

--- BE-WOK-platform/API/DTOs/Reservations/ReservationPostModel.cs
+++ BE-WOK-platform/API/DTOs/Reservations/ReservationPostModel.cs
@@ -3,6 +3,10 @@
     public class ReservationPostModel
     {
         public int TableNumber { get; set; }
+        public int NoOfPeople { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Details { get; set; }
         public DateTime ReservationTime { get; set; }
     }
 }
---
+++ BE-WOK-platform/Infrastructure/Migrations/20230326131916_ExtendedReservationModel.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendedReservationModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NoOfPeople",
+                table: "Reservations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Details",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "NoOfPeople",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Reservations");
+        }
+    }
+}
