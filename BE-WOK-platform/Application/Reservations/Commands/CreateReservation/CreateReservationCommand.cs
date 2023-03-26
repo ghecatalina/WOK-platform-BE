2813abd33a00aadd6ad697bd4df9a3a99457f67c
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-26 16:19:57 +0300

    extended reservation model

--- BE-WOK-platform/Application/Reservations/Commands/CreateReservation/CreateReservationCommand.cs
+++ BE-WOK-platform/Application/Reservations/Commands/CreateReservation/CreateReservationCommand.cs
@@ -6,6 +6,10 @@ namespace Application.Reservations.Commands.CreateReservation
     public class CreateReservationCommand : IRequest<Reservation>
     {
         public int TableNumber { get; set; }
+        public int NoOfPeople { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Details { get; set; }
         public DateTime ReservationTime { get; set; }
     }
 }
--- BE-WOK-platform/Domain/Models/Reservation.cs
+++ BE-WOK-platform/Domain/Models/Reservation.cs
@@ -4,6 +4,10 @@
     {
         public int Id { get; set; }
         public int TableNumber { get; set; }
+        public int NoOfPeople { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Details { get; set; }
         public DateTime ReservationTime { get; set; }
     }
 }
